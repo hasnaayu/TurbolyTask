de4325f2538748b1946dc497591e94e378834000
Hasna <hasnaayuroida@gmail.com>
2024-06-27 13:00:45 +0700

    main page

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,63 +2,30 @@
     <div class="d-flex flex-column flex-row-fluid">
         <div class="container-fluid bg-white">
             <div class="d-flex justify-content-between">
-                <div>
-                    <div class="d-flex flex-row align-items-center">
-                        <div>
-                            <a href="/official-store">
-                                <img src="{{ asset('img/official-store.png') }}" class="d-block w-lg-50px w-30px mr-3"
-                                    alt="logo">
-                            </a>
-                        </div>
-                        <div class="mt-4">
-                            <p class="font-size-h4-lg font-size-h6 base_color">Ninuninu Official Store</p>
-                        </div>
-                    </div>
+                <div class="d-flex justify-content-start align-items-center">
+                    <a href="/home">
+                        <img src="{{ asset('img/logo.png') }}" class="w-lg-120px w-60px mt-4" alt="" />
+                    </a>
                 </div>
                 <div class="d-flex flex-row align-items-center">
-                    <p class="mr-5 font-size-h4-lg font-size-h5 mt-4 date_web">
+                    <p class="font-size-h3-lg font-size-h5 mr-5 mt-4 date_web">
                         {{ \Carbon\Carbon::today()->translatedFormat('l, d F Y') }}
                     </p>
-                    <p class="font-size-h4-lg font-size-h5 mr-5 mt-4 time_web" id="time"></p>
-                    <div class="vertical mr-5 mt-2" style="border-left: 2px solid black;
-            height: 50px;">
+                    <p class="font-size-h3-lg font-size-h5 mr-5 mt-4 time_web" id="time"></p>
+                    <div class="vertical mr-5 mt-2 vertical-line">
                     </div>
-                    <div>
-                        <div class="dropdown open">
-                            <div class="profile_btn_web btn btn-outline-primary px-3 py-2 mt-2 bg-white base_color"
-                                style="background-color:white; color: #58B0E2" type="button" id="dropdownMenu3"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                {{ Auth::user()->name }}
-                            </div>
-                            <div class="dropdown-menu">
-                                <a class="dropdown-item" href="/official-store/finance/tunai">Keuangan</a>
-                                <a class="dropdown-item"
-                                    href="/official-store/transaction/berhasil-dibayar">Transaksi</a>
-                                <a class="dropdown-item" href="/official-store/logout">Keluar</a>
-                            </div>
+                    <div class="dropdown">
+                        <div class="btn-outline-primary mt-2" type="button" id="dropdownMenu3"
+                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            {{ Auth::user()->name }}
+                        </div>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenu3">
+                            <a class="dropdown-item" href="/auth/logout">Keluar</a>
                         </div>
                     </div>
                 </div>
             </div>
             <hr class="top_wavy_bg" style="margin-bottom: 0% !important">
-        </div>
-
-        <div class="container-fluid bg-white">
-            <div class="d-flex flex-row align-items-center py-4">
-                <a href="/home">
-                    <img src="{{ asset('img/logo.png') }}" class="w-lg-120px w-60px mr-2" alt="" />
-                </a>
-                <div class="d-flex mr-1 typeahead">
-                    <input class="form-control search_bar_input"
-                        value="{{ request()->get('search') == null ? null : request()->get('search') }}" type="text"
-                        placeholder="Search..." name="search_field_web" id="search_product_web" />
-                    <div class="input-group-append">
-                        <span class="input-group-text bg_color text-white" id="search_web"
-                            style="cursor: pointer;">Cari</span>
-                    </div>
-                </div>
-            </div>
-            <hr style="margin-top: 0% !important">
         </div>
     </div>
 </div>
--- resources/views/partials/layout.blade.php
+++ resources/views/partials/layout.blade.php
@@ -10,38 +10,15 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
-    {{-- <link href="{{ asset('assets/css/auth.css') }}" rel="stylesheet" type="text/css" /> --}}
+    <link href="{{ asset('assets/css/home.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.2/bootstrap3-typeahead.min.css">
 
     <link rel="shortcut icon" href="{{ asset('img/logo_tab.png') }}" />
-
-    <style>
-        .se-pre-con {
-            position: fixed;
-            display: block;
-            width: 100%;
-            height: 100%;
-            top: 0;
-            left: 0;
-            text-align: center;
-            opacity: 1;
-            background-color: #fff;
-            z-index: 99;
-        }
-
-        #loading_item {
-            position: fixed;
-            top: 50%;
-            left: 50%;
-            z-index: 100;
-        }
-
-        .base_color {
-            color: #58B0E2;
-        }
-    </style>
 </head>
 
 <body>
@@ -62,11 +39,75 @@
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://twitter.github.io/typeahead.js/releases/latest/typeahead.bundle.js"></script>
 
     <script type="text/javascript">
         $(window).on('load', function() {
             $(".se-pre-con").fadeOut("slow");
+        });
+
+        window.onload = function() {
+            clock();
+
+            function clock() {
+                var now = new Date();
+                var TwentyFourHour = now.getHours();
+                var hour = now.getHours();
+                var min = now.getMinutes();
+                var sec = now.getSeconds();
+                var mid = 'PM';
+                if (min < 10) {
+                    min = "0" + min;
+                }
+                if (hour > 12) {
+                    hour = hour - 12;
+                }
+                if (hour == 0) {
+                    hour = 12;
+                }
+                if (TwentyFourHour < 12) {
+                    mid = 'AM';
+                }
+                document.getElementById('time').innerHTML = hour + ':' + min + ' ' + mid;
+                setTimeout(clock, 1000);
+            }
+        }
+
+        $(document).ready(function() {
+            var substringMatcher = function(strs) {
+                return function findMatches(q, cb) {
+                    var matches, substringRegex;
+
+                    matches = [];
+
+                    substrRegex = new RegExp(q, 'i');
+
+                    $.each(strs, function(i, str) {
+                        if (substrRegex.test(str)) {
+                            matches.push(str);
+                        }
+                    });
+
+                    cb(matches);
+                };
+            };
+            var response = new Bloodhound({
+                datumTokenizer: Bloodhound.tokenizers.whitespace,
+                queryTokenizer: Bloodhound.tokenizers.whitespace,
+                remote: {
+                    url: window.location.origin + '/home/task/autocomplete?search=%QUERY',
+                    wildcard: '%QUERY'
+                }
+            });
+
+            $('#search').typeahead({
+                hint: true,
+                highlight: true,
+                minLength: 1
+            }, {
+                name: 'response',
+                source: response
+            });
         });
     </script>
 </body>
